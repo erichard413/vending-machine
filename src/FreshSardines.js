afae637cbf53e518361f526108bc1c92207d226f
Erik Richard <erichard413@outlook.com>
2023-01-28 18:57:02 -0500

    Fixing URL error

--- src/FreshSardines.js
+++ src/FreshSardines.js
@@ -7,7 +7,7 @@ const FreshSardines = () => {
         <div className="FreshSardines">
             <div className="FreshSardines-message">
                 <p>YOU DON'T EAT SARDINES. THEY EAT YOU</p>
-                <NavLink className="FreshSardines-link" to='/vending-machines'>GO BACK</NavLink>
+                <NavLink className="FreshSardines-link" to='/vending-machine'>GO BACK</NavLink>
             </div>
         </div>
     )
